5b80ca2082c19d8f14846d5b3bee421597ee2487
dinho <99069865@mydavinci.nl>
2023-05-09 13:18:03 +0200

    'update'

--- module1/ruit.js
+++ module1/ruit.js
@@ -1,12 +1,23 @@
+let alert = Number(prompt("geef een getal: ")) +2
 
-function ruit(){
-    let line = ""
-    let nummer = document.getElementById("nummer").value
-    for(let i=1; i<= nummer; i++){
-        if (i > 1 ){
-            line += "-";}
-        line += i
-         
-        console.log(line)
+for (let i = 0; i < alert; i++){
+    const nummer = []
+    for (let z = 1; z < i; z++){
+        nummer.push(z)
     }
+    let test = nummer.join("-")
+    document.write(test)
+    document.write("<br>")
+}
+
+alert = alert - 3
+
+for (let i = alert; i > 0; i--){
+    const nummer = []
+    for (let z = i; z > 0; z--){
+        nummer.push(z)
+    }
+    let test2 = nummer.reverse()
+    document.write(test2.join("-"))
+    document.write("<br>")
 }
