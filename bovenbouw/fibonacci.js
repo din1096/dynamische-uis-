512df5ee665109a3533fe22f4cc6199f753e9350
dinho <99069865@mydavinci.nl>
2024-03-25 11:39:09 +0100

    'update;

---
+++ bovenbouw/fibonacci.js
@@ -0,0 +1,18 @@
+function fibonacci(x) {
+    let a = 0;
+    let b = 1;
+    let reeks = [a, b];
+    for (let i = 0; i < x - 2; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+        reeks.push(b);
+    }
+    let guldensnee = b / a;
+    return [reeks, guldensnee];
+}
+document.getElementById("antwoord").innerText = fibonacci;
+let aantal = parseInt(prompt("voer een getal in:"));
+alert(aantal);
+console.log(fibonacci(aantal));
+
